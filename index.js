d94f5fd0cb85c05d8682e53872504eb9f25cfd1b
Gustavo Nicolaci <gustavo_nicolaci@hotmail.com>
2023-09-13 15:58:33 -0300

    ap01(js): previsao do tempo com then/catch

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const axios = require('axios')
 // console.log(process.env)
 // console.log(new Date(1694034000 * 1000).toLocaleString())
 
-
 // const appid = process.env.appid
 // const q='Itu' = process.env.q
 // const units = process.env.units
@@ -17,5 +16,24 @@ const {appid, q, units, cnt, language, url} = process.env
 
 const end = `${url}?appid=${appid}&q=${q}&units=${units}&cnt=${cnt}&lang=${language}`
 
-
-console.log(end)
+axios.get(end).then((res) => {
+    console.log(res['data'])
+    console.log("======================================================")
+    return res['data']
+})
+.then(res => {
+    console.log(res.list)
+    console.log("================================================================")
+    return res.list
+}) 
+.then(res => {
+    //fazer um for sobre o res que exiba o dt de cada previsao
+    //exiba a temperatura minima e a temperatura maxima
+    for (let previsao of res) {
+        console.log(new Date (+previsao.dt * 1000).toLocaleString())
+        console.log(`Temperatura mínima: ${previsao.main.temp_min}`)
+        console.log(`Temperatura máxima: ${previsao.main.temp_max}`)
+        console.log('\n')
+    }
+    console.log("==============================================================")
+})
